d8ccd707884e68297d139fbdde69d8c1455eb3fb
shirley <shirleymissias20@gmail.com>
2024-03-05 17:58:18 -0300

    Avanço do exercicio de IMC

--- exer13Kindle/script.js
+++ exer13Kindle/script.js
@@ -4,12 +4,35 @@ let peso = document.getElementById("peso");
 let altura = document.getElementById("altura");
 let resultado = document.getElementById("resultado");
 
+    resultado.style.marginTop = "20px";
+    resultado.style.width = "350px";
+    resultado.style.borderRadius = "10px";
+    resultado.style.padding = "10px";
+    resultado.style.fontSize = "18px";
+    resultado.style.fontWeight = "700";
+
+    limpar.addEventListener('click', () => {
+      peso.reset()
+    })
+
 calculo.addEventListener('click', () => {
-  const nPeso = Number(peso.value);
-  const nAltura = Number(altura.value);
-  const calcular = nPeso / nAltura;
-
-  if()
-  resultado.innerText = "resultado" + calcular;
-
+  const nPeso = parseFloat(peso.value);
+  const nAltura = parseFloat(altura.value);
+  const calcular = (nPeso / (nAltura * nAltura)).toFixed(2);
+  
+  if (!calcular) {
+    resultado.textContent = "ERROR Insira um valor"  
+  } else if ( calcular <= 18.5) {
+    resultado.textContent = "O resultado é " + calcular + " Você está muito abaixo do peso"  
+  } else if (calcular > 18.5 && calcular <= 24.9) {
+    resultado.textContent = "O resultado é " + calcular + " Você está com o peso Normal" 
+  } else if (calcular > 24.9 && calcular <= 29.9) {
+    resultado.textContent = "O resultado é " + calcular + " Você está com Sobrepeso" 
+  } else if (calcular > 29.9  && calcular <= 34.9) {
+    resultado.textContent = "O resultado é " + calcular + " Você está com Obesidade Grau I" 
+  } else if (calcular > 34.9  && calcular <= 39.9) {
+    resultado.textContent = "O resultado é " + calcular + " Você está com Obesidade Grau II" 
+  } else if (calcular > 40) {
+    resultado.textContent = "O resultado é " + calcular + " Você está com Obesidade Grau III" 
+  }
 })
